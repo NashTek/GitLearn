789d4d6f1cf2c8a53b51591a63531cd61f7b81dc
Jonathon Aslanian <jaslanian@protonmail.com>
2023-07-24 10:45:04 -0600

    Create script.ps1
    feat(ActiveDirectory): add user inactivity and status script
    
    This commit adds a new PowerShell script (script.ps1) to the repository. The script searches an Active Directory environment for user accounts that have not logged in for 60 days, or are disabled. The script provides a report of such user accounts and suggests potential remediation actions.

---
+++ script.ps1
@@ -0,0 +1,12 @@
+# Import the required module
+Import-Module ActiveDirectory
+
+$thresholdDate = (Get-Date).AddDays(-60)
+$inactiveUsers = Get-ADUser -Filter {(LastLogonDate -lt $thresholdDate) -or (Enabled -eq $false)} -Properties LastLogonDate, Enabled
+
+# Output report
+$inactiveUsers | Format-Table Name, LastLogonDate, Enabled
+
+# Suggest remediation
+Write-Host "The above users have been inactive for 60 days or more, or are disabled."
+Write-Host "Consider following up with these users or their managers to determine if their accounts can be deleted or further action needs to be taken."
