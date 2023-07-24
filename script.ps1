b2ad39706d476bb2863ca09055108bf5c709e957
Jon Aslanian <Jaslanian@protonmail.com>
2023-07-24 12:01:55 -0600

    aDDED LINES

--- script.ps1
+++ script.ps1
@@ -12,3 +12,8 @@ Write-Host "The above users have been inactive for 60 days or more, or are disab
 Write-Host "Consider following up with these users or their managers to determine if their accounts can be deleted or further action needs to be taken."
 
 Write-Host "Press any key to continue..."
+Write-Host "Press any key to continue..."
+Write-Host "Press any key to continue..."
+Write-Host "Press any key to continue..."
+Write-Host "Press any key to continue..."
+Write-Host "Press any key to continue..."
